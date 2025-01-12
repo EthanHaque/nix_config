9e0eed7c580ccee45bc3799709dc549d5b188e7c
Ethan Haque <ethan.k.haque@gmail.com>
2025-01-12 08:56:17 -0500

    fix: setting default cmdheight to 0

--- home-manager/modules/neovim/nvim/config/options.lua
+++ home-manager/modules/neovim/nvim/config/options.lua
@@ -49,6 +49,7 @@ vim.opt.listchars = {
 }
 
 -- Removes command line from bottom except when recording macro.
+vim.opt.cmdheight = 0
 vim.cmd [[ autocmd RecordingLeave * set cmdheight=0 ]]
 vim.cmd [[ autocmd RecordingEnter * set cmdheight=1 ]]
 
