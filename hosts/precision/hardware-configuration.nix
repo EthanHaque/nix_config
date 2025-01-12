5e2b4d5318a4afcf2f6e5340e60582ccd2e625e7
Ethan Haque <ethan.k.haque@gmail.com>
2025-01-12 04:36:03 -0500

    fix: updating hardware.opengl to hardware.graphics

--- hosts/precision/hardware-configuration.nix
+++ hosts/precision/hardware-configuration.nix
@@ -34,6 +34,6 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  hardware.opengl.enable = true;
+  hardware.graphics.enable = true;
   hardware.nvidia.modesetting.enable = true;
 }
