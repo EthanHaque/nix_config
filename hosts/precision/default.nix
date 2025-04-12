c02f38de4009035572a54bfaae0956ed959583bd
Ethan Haque <ethan.k.haque@gmail.com>
2025-04-11 22:20:29 -0400

    fix: remove commented packages

--- hosts/precision/default.nix
+++ hosts/precision/default.nix
@@ -82,37 +82,15 @@
   environment.gnome.excludePackages = with pkgs; [
     orca
     evince
-    # file-roller
     geary
-    # gnome-disk-utility
-    # seahorse
-    # sushi
-    # sysprof
-    #
-    # gnome-shell-extensions
-    #
-    # adwaita-icon-theme
-    # nixos-background-info
     gnome-backgrounds
-    # gnome-bluetooth
-    # gnome-color-manager
-    # gnome-control-center
-    # gnome-shell-extensions
-    gnome-tour # GNOME Shell detects the .desktop file on first log-in.
+    gnome-tour
     gnome-user-docs
-    # glib # for gsettings program
-    # gnome-menus
-    # gtk3.out # for gtk-launch program
-    # xdg-user-dirs # Update user dirs as described in https://freedesktop.org/wiki/Software/xdg-user-dirs/
-    # xdg-user-dirs-gtk # Used to create the default bookmarks
-    #
     baobab
     epiphany
     gnome-text-editor
     gnome-calculator
     gnome-calendar
-    # gnome-characters
-    # gnome-clocks
     gnome-console
     gnome-contacts
     gnome-font-viewer
@@ -121,12 +99,9 @@
     gnome-music
     gnome-system-monitor
     gnome-weather
-    # loupe
-    # nautilus
     gnome-connections
     simple-scan
     snapshot
-    # totem
     yelp
     gnome-software
   ];
