8bd33a335c1fd1b5d30f4c4152a2ff9a1c30ab83
Ethan Haque <ethan.k.haque@gmail.com>
2025-04-11 22:22:37 -0400

    fix: xterm is not removed without hack

--- hosts/precision/default.nix
+++ hosts/precision/default.nix
@@ -29,7 +29,7 @@
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.desktopManager.xterm.enable = false;
+  services.xserver.excludePackages = [ pkgs.xterm ];
   services.xserver.xkb = {
     layout = "us";
     variant = "";
