9c4ab9eff356eae45b37c265c584c64f2b4e5a94
Ethan Haque <ethan.k.haque@gmail.com>
2025-07-09 05:25:59 -0400

    feat: add hyprlock

--- home-manager/modules/hyprland/default.nix
+++ home-manager/modules/hyprland/default.nix
@@ -23,6 +23,99 @@ in
     };
   };
 
+  programs.hyprlock = {
+    enable = true;
+
+    settings = {
+      general = {
+        disable_loading_bar = true;
+        immediate_render = true;
+        hide_cursor = false;
+        no_fade_in = true;
+      };
+
+      animation = [
+        "inputFieldDots, 1, 2, linear"
+        "fadeIn, 0"
+      ];
+
+      background = [
+        {
+          monitor = "";
+          path = "${wallpaper}";
+          blur_passes = 2;
+          contrast = 1;
+          brightness = 0.5;
+          vibrancy = 0.2;
+        }
+      ];
+
+      input-field = [
+        {
+          monitor = "";
+
+          size = "300, 50";
+          valign = "bottom";
+          position = "0%, 10%";
+
+          outline_thickness = 1;
+
+          font_color = "rgb(0f4a0a)";
+          outer_color = "rgba(180, 180, 180, 0.5)";
+          inner_color = "rgba(200, 200, 200, 0.1)";
+          check_color = "rgba(247, 193, 19, 0.5)";
+          fail_color = "rgba(255, 106, 134, 0.5)";
+
+          fade_on_empty = false;
+          placeholder_text = "Enter Password";
+
+          dots_spacing = 0.2;
+          dots_center = true;
+          dots_fade_time = 100;
+
+          shadow_color = "rgba(0, 0, 0, 0.1)";
+          shadow_size = 7;
+          shadow_passes = 2;
+        }
+      ];
+
+      label = [
+        {
+          monitor = "";
+          text = "$TIME";
+          font_size = 150;
+          color = "rgb(0f4a0a)";
+
+          position = "0%, 30%";
+
+          valign = "center";
+          halign = "center";
+
+          shadow_color = "rgba(0, 0, 0, 0.1)";
+          shadow_size = 20;
+          shadow_passes = 2;
+          shadow_boost = 0.3;
+        }
+        {
+          monitor = "";
+          text = "cmd[update:3600000] date +'%a %b %d'";
+          font_size = 20;
+          color = "rgb(0f4a0a)";
+
+          position = "0%, 40%";
+
+          valign = "center";
+          halign = "center";
+
+          shadow_color = "rgba(0, 0, 0, 0.1)";
+          shadow_size = 20;
+          shadow_passes = 2;
+          shadow_boost = 0.3;
+        }
+      ];
+    };
+  };
+
   wayland.windowManager.hyprland = {
     enable = true;
     package = null;
@@ -137,6 +230,7 @@ in
         "$mainMod, T, exec, ${terminal}"
         "$mainMod, F, exec, ${browser}"
         "$mainMod, B, exec, ${fileManager}"
+        "$mainMod SHIFT, L, exec, hyprlock"
         "$mainMod, W, killactive,"
         "$mainMod, M, exit,"
         "$mainMod, V, togglefloating,"
