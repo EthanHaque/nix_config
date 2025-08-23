48706ae4b9672e68f22294dee40e3f6e8c4bd953
Ethan Haque <ethan.k.haque@gmail.com>
2025-08-23 08:14:47 -0400

    feat: simplify neovim build

--- home-manager/modules/neovim/default.nix
+++ home-manager/modules/neovim/default.nix
@@ -11,90 +11,21 @@
     extraPackages = with pkgs; [
       gcc
       xclip
-      # Telescope
       ripgrep
-
-
       lua-language-server
       stylua
-      pyright
+      pyrefly
       eslint
+      ruff
       vtsls
     ];
-
-    plugins = with pkgs.vimPlugins; [
-      lazy-nvim
-    ];
-
-    extraLuaConfig =
-      let
-        plugins = with pkgs.vimPlugins; [
-          nvim-cmp
-          cmp-nvim-lsp
-          cmp-buffer
-          cmp-path
-          cmp_luasnip
-          luasnip
-          friendly-snippets
-          plenary-nvim
-          nvim-lspconfig
-
-          nvim-autopairs
-
-          cyberdream-nvim
-
-          comment-nvim
-
-          gitsigns-nvim
-
-          lualine-nvim
-
-          telescope-nvim
-          telescope-fzf-native-nvim
-
-          toggleterm-nvim
-
-          nvim-treesitter
-          nvim-treesitter-context
-          nvim-treesitter-textobjects
-          nvim-treesitter.withAllGrammars
-
-          nvim-surround
-        ];
-        mkEntryFromDrv = drv:
-          if lib.isDerivation drv then
-            { name = "${lib.getName drv}"; path = drv; }
-          else
-            drv;
-        lazyPath = pkgs.linkFarm "lazy-plugins" (builtins.map mkEntryFromDrv plugins);
-      in
-      ''
-        require("lazy").setup({
-          defaults = {
-            lazy = true,
-          },
-          dev = {
-            -- reuse files from pkgs.vimPlugins.*
-            path = "${lazyPath}",
-            patterns = { "" },
-            -- fallback to download
-            fallback = true,
-          },
-          spec = {
-            -- force enable telescope-fzf-native.nvim
-            { "nvim-telescope/telescope-fzf-native.nvim", enabled = true },
-            -- import/override with your plugins
-            { import = "plugins" },
-            -- treesitter handled by xdg.configFile."nvim/parser", put this line at the end of spec to clear ensure_installed
-            { "nvim-treesitter/nvim-treesitter", opts = { ensure_installed = {} } },
-	    -- disable mason.nvim, use programs.neovim.extraPackages
-            { "williamboman/mason-lspconfig.nvim", enabled = false },
-            { "williamboman/mason.nvim", enabled = false },
-          },
-        })
-      '';
   };
 
-
-  xdg.configFile."nvim".source = ./lazy;
+  # This line now handles everything else.
+  xdg.configFile."nvim".source = pkgs.fetchFromGitHub {
+    owner = "EthanHaque";
+    repo = "nvim_lazy_config";
+    rev = "25d5d9aa31a50e5e537c3b1302d904c56b38f180";
+    sha256 = "8JiufR+NdRqEZy8HG4RPUEPE/OkF28sffMnyLbYkYzY=";
+  };
 }
