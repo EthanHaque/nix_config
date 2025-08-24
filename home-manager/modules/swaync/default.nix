751153ee3cc8a2870c4b1842fe5f2b749483bbb9
Ethan Haque <ethan.k.haque@gmail.com>
2025-08-24 06:15:05 -0400

    fix: restore old swaync config

--- home-manager/modules/swaync/default.nix
+++ home-manager/modules/swaync/default.nix
@@ -31,16 +31,27 @@ config,
       timeout-critical = 0;
       notification-window-width = 500;
       keyboard-shortcuts = true;
+      image-visibility = "when-available";
       transition-time = 200;
       hide-on-clear = true;
       hide-on-action = true;
       script-fail-notify = true;
       widgets = [
+        "menubar"
+        "buttons-grid"
+        "volume"
+        "mpris"
+        "inhibitors"
         "title"
         "dnd"
         "notifications"
       ];
       widget-config = {
+        inhibitors = {
+          text = "Inhibitors";
+          button-text = "Clear All";
+          clear-all-button = true;
+        };
         title = {
           text = "Notifications";
           clear-all-button = true;
@@ -48,6 +59,67 @@ config,
         };
         dnd = {
           text = "Do Not Disturb";
+        };
+        mpris = {
+          image-size = 96;
+          image-radius = 6;
+        };
+        volume = {
+          label = "🔊";
+        };
+        menubar = {
+          "menu#power-buttons" = {
+            label = "";
+            position = "right";
+            actions = [
+              {
+                label = "  Reboot";
+                command = "systemctl reboot";
+              }
+              {
+                label = "  Lock";
+                command = "hyprlock";
+              }
+              {
+                label = "  Logout";
+                command = "hyprctl dispatch exit";
+              }
+              {
+                label = "  Shut down";
+                command = "systemctl poweroff";
+              }
+            ];
+          };
+          "menu#dotfiles-buttons" = {
+            label = " ";
+            position = "left";
+            actions = [
+              {
+                label = "  My Github";
+                command = "firefox https://github.com/EthanHaque";
+              }
+            ];
+          };
+        };
+        "buttons-grid" = {
+          actions = [
+            {
+              label = " ";
+              command = "kitty nmtui";
+            }
+            {
+              label = "";
+              command = "blueman-manager";
+            }
+            {
+              label = "";
+              command = "firefox";
+            }
+            {
+              label = "󱂵";
+              command = "nautilus";
+            }
+          ];
         };
       };
     };
