80163da849daaa8a7d3b8d31ae66da599df1a295
Ethan Haque <ethan.k.haque@gmail.com>
2025-08-24 04:37:05 -0400

    chore: update renamed items

--- hosts/precision/default.nix
+++ hosts/precision/default.nix
@@ -27,8 +27,8 @@
   };
 
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
   services.xserver.excludePackages = [ pkgs.xterm ];
   services.xserver.xkb = {
     layout = "us";
