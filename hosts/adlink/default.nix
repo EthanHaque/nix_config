b2f73d404615101e89f2c503ea1986c40af0a727
Ethan Haque <ethan.k.haque@gmail.com>
2025-09-10 22:08:03 -0400

    Feat/adlink homelab (#5)
    * feat: set powerManagement to true
    
    * feat: enable gpu support in containers
    
    * fix: add missing semicolon
    
    * fix: disable nvidia powerManagement
    
    * fix: replace deprecated enableNvidia with nvidia-container-toolkit
    
    * feat: enable ssh
    
    * feat: remove gui
    
    * feat: use wired ethernet connection in home instead of wifi
    
    * feat: add firewall
    
    * feat: remove passwordAuthentication over ssh
    
    * fix: add missing semicolon
    
    * feat: add minecraft server

--- hosts/adlink/default.nix
+++ hosts/adlink/default.nix
@@ -1,9 +1,68 @@
-{ config, pkgs, lib,... }:
+{ config, pkgs, lib, inputs, ... }:
 {
   imports =
     [
       ./hardware-configuration.nix
+      inputs.nix-minecraft.nixosModules.minecraft-servers
     ];
+
+  nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
+
+  services.minecraft-servers = {
+    enable = true;
+    eula = true;
+    openFirewall = true;
+    servers.modded = {
+      enable = true;
+      package = pkgs.fabricServers.fabric-1_21_8;
+      jvmOpts = "-Xms8G -Xmx8G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true";
+      serverProperties = {
+        gamemode = "survival";
+        difficulty = "hard";
+        motd = "The gang's Minecraft server";
+        max-players = 10;
+        simulation-distance = 16;
+        view-distance = 16;
+      };
+      operators = {
+        bbeeath = "e82e11fa-74f8-4958-b1fa-e9f4d1357c95";
+      };
+      symlinks = {
+        mods = pkgs.linkFarmFromDrvs "mods" (
+          builtins.attrValues {
+            c2me = pkgs.fetchurl {
+              url = "https://cdn.modrinth.com/data/VSNURh3q/versions/tlZRTK1v/c2me-fabric-mc1.21.8-0.3.4.0.0.jar";
+              sha512 = "MMvFIMuDSQNtVaHLHyaWTPAkEM9talYdnMBxZNdWajp1ZDZ95iUQ8rq1ByPCx8QBcYABFT+oM1YGNM5LLiEnZw==";
+            };
+            chunky = pkgs.fetchurl {
+              url = "https://cdn.modrinth.com/data/fALzjamp/versions/inWDi2cf/Chunky-Fabric-1.4.40.jar";
+              sha512 = "ngOG0DJkGhJP2VOmiKSAZt9/TsEYb38PiwpW1J3O0iDi1pOO1W6djq14u4DduUG8eHP1g63Y5WW9rN9i4TrcKA==";
+            };
+            fabric-api = pkgs.fetchurl {
+              url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/Q8ssLFZp/fabric-api-0.133.0%2B1.21.8.jar";
+              sha512 = "BKsr0pgO86ygB2NnbRMokh4aixUxcS6nyot02u/Cu5hveME6LIIa5laFdOS96zyaMWrs3URZotQmRlCMdHjCbw==";
+            };
+            ferrite-core = pkgs.fetchurl {
+              url = "https://cdn.modrinth.com/data/uXXizFIs/versions/CtMpt7Jr/ferritecore-8.0.0-fabric.jar";
+              sha512 = "ExuC0dNm8JZkNb/LOMNi1gTWjs8wwQbTGmJhv8hoyjqCQluz+uuqLl6hfY7tXJKEOBDrLfR5Dy+LHmwb3Jt3RQ==";
+            };
+            lithium = pkgs.fetchurl {
+              url = "https://cdn.modrinth.com/data/gvQqBUqZ/versions/pDfTqezk/lithium-fabric-0.18.0%2Bmc1.21.8.jar";
+              sha512 = "bGmVB2D0jviPDFhx5hAptZrwOrXtmwArakcNet/fJvC4ddzTYLZk6JcpEAJTCYHCDgsokPuInyns2qAH+IUQDw==";
+            };
+            scalable-lux = pkgs.fetchurl {
+              url = "https://cdn.modrinth.com/data/Ps1zyz6x/versions/Bi5i8Ema/ScalableLux-0.1.5.1%2Bfabric.abdeefa-all.jar";
+              sha512 = "Qh4WkejZUG3vSJELsVyZQT6vabHE/ltyn1E/TC4c0l3bgVU5fpyeurNTznKFCnymJhnIX90G05vIfPp1IK8CgQ==";
+            };
+            spark = pkgs.fetchurl {
+              url = "https://cdn.modrinth.com/data/l6YH9Als/versions/3KCl7Vx0/spark-1.10.142-fabric.jar";
+              sha512 = "lbfk8kFuIKv52d9B/LzgTyjr8KoIY3R0JlJ4mohkLdaCDIiEqyQDNFVTRbScOffQyvI9UhzslRaZHvQ7okdYrw==";
+            };
+          }
+        );
+      };
+    };
+  };
 
   boot.loader.systemd-boot.enable = false;
   boot.loader.efi.canTouchEfiVariables = false;
@@ -15,7 +74,17 @@
 
 
   networking.hostName = "gluon";
-  networking.networkmanager.enable = true;
+  networking.networkmanager.enable = false;
+  networking.useDHCP = false;
+  networking.interfaces."enP2s1f0np0".useDHCP = true;
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [
+      22
+      80
+    ];
+  };
 
   time.timeZone = "America/New_York";
   i18n.defaultLocale = "en_US.UTF-8";
@@ -31,72 +100,34 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  services.xserver.enable = true;
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
-  services.xserver.excludePackages = [ pkgs.xterm ];
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
 
   hardware.graphics.enable = true;
-
-  # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"];
 
   hardware.nvidia = {
-    # Modesetting is required.
     modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the bare essentials.
     powerManagement.enable = false;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
     powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
     open = true;
-
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
     nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
 
-  services.printing.enable = true;
-
-  virtualisation.docker.enable = true;
-
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  security.polkit.enable = true;
-  services.pipewire = {
+  virtualisation.docker= {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
   };
+  hardware.nvidia-container-toolkit.enable = true;
 
   users.users.muon = {
     isNormalUser = true;
     description = "muon";
     extraGroups = [ "networkmanager" "wheel" "docker" ];
-    packages = with pkgs; [ ];
+  };
+
+  services.openssh  = {
+    enable = true;
+    passwordAuthentication = false;
   };
 
   nix.settings = {
@@ -112,12 +143,6 @@
     stalled-download-timeout = 500;
   };
 
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
-    ];
-  };
 
   nixpkgs.config = {
     allowUnfree = true;
@@ -142,33 +167,5 @@
     enableSSHSupport = true;
   };
 
-  environment.gnome.excludePackages = with pkgs; [
-    orca
-    evince
-    geary
-    gnome-backgrounds
-    gnome-tour
-    gnome-user-docs
-    baobab
-    epiphany
-    gnome-text-editor
-    gnome-calculator
-    gnome-calendar
-    gnome-console
-    gnome-contacts
-    gnome-font-viewer
-    gnome-logs
-    gnome-maps
-    gnome-music
-    gnome-system-monitor
-    gnome-weather
-    gnome-connections
-    simple-scan
-    snapshot
-    yelp
-    gnome-software
-  ];
-
   system.stateVersion = "24.11";
-
 }
