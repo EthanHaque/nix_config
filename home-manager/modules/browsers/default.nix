0c488e2e69211e44006e22407e71807620ac75b0
Ethan Haque <ethan.k.haque@gmail.com>
2025-10-06 00:16:31 -0400

    feat: add separate work firefox profile

--- home-manager/modules/browsers/default.nix
+++ home-manager/modules/browsers/default.nix
@@ -1,17 +1,32 @@
 {
-pkgs,
-config,
-inputs,
-...
-}: {
+  pkgs,
+  config,
+  inputs,
+  ...
+}:
+let
+  commonExtensions = with inputs.firefox-addons.packages.${pkgs.system}; [
+    ublock-origin
+    tridactyl
+    noscript
+  ];
+in
+{
   programs.firefox = {
     enable = true;
-    profiles.default = {
-      extensions.packages = with inputs.firefox-addons.packages.${pkgs.system}; [
-        ublock-origin
-        tridactyl
-        noscript
-      ];
+
+    profiles = {
+      default = {
+        id = 0;
+        isDefault = true;
+        extensions.packages = commonExtensions;
+      };
+
+      tradespace = {
+        id = 1;
+        isDefault = false;
+        extensions.packages = commonExtensions;
+      };
     };
   };
 }
