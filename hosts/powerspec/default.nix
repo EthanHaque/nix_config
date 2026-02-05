abf6ba3328141fabc1ed4628798b950316898e5c
Ethan Haque <ethan.k.haque@gmail.com>
2026-02-04 22:12:46 -0500

    fix: clean up powerspec configuration

--- hosts/powerspec/default.nix
+++ hosts/powerspec/default.nix
@@ -4,71 +4,18 @@
   imports =
     [
       ./hardware-configuration.nix
+      ../../modules/nixos/core.nix
+      ../../modules/nixos/workstation.nix
+      ../../modules/nixos/gnome.nix
     ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "tau"; # Define your hostname.
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nix.settings.trusted-users = [ "neutrino" ];
-
-
   networking.networkmanager.enable = true;
-  time.timeZone = "America/New_York";
 
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
-  services.xserver.windowManager.i3.enable = true;
-  services.xserver.excludePackages = [ pkgs.xterm ];
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
   services.xserver.videoDrivers = ["nvidia"];
-
-  environment.gnome.excludePackages = with pkgs; [
-    orca
-    evince
-    geary
-    gnome-backgrounds
-    gnome-tour
-    gnome-user-docs
-    baobab
-    epiphany
-    gnome-text-editor
-    gnome-calculator
-    gnome-calendar
-    gnome-console
-    gnome-contacts
-    gnome-font-viewer
-    gnome-logs
-    gnome-maps
-    gnome-music
-    gnome-system-monitor
-    gnome-weather
-    gnome-connections
-    simple-scan
-    snapshot
-    yelp
-    gnome-software
-  ];
 
   programs.sway.enable=true;
   programs.sway.extraOptions = [ "--unsupported-gpu" ];
@@ -79,9 +26,6 @@
     modesetting.enable = true;
     powerManagement.enable = false;
     powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
     open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.beta;
@@ -91,50 +35,13 @@
 
   virtualisation.docker.enable = true;
 
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  users.users.neutrino = {
+  users.users.${vars.username} = {
     isNormalUser = true;
-    description = "neutrino";
     extraGroups = [ "networkmanager" "wheel" "docker" "input" ];
     packages = with pkgs; [ ];
   };
 
-  programs.nano.enable = false;
   programs.steam.enable = true;
 
-  nixpkgs.config.allowUnfree = true;
-
-  environment.variables.EDITOR = "vim";
-  environment.systemPackages = with pkgs; [
-    vim
-    docker-compose
-    pinentry-curses
-  ];
-
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-  ];
-
-  services.pcscd.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
-
+  system.stateVersion = "25.05";
 }
