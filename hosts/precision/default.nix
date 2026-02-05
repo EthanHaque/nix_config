a6e57e246ad9731d84c689d380eda69ba9b58c3b
Ethan Haque <ethan.k.haque@gmail.com>
2026-02-04 21:29:38 -0500

    fix: simplify precision configuration

--- hosts/precision/default.nix
+++ hosts/precision/default.nix
@@ -2,32 +2,17 @@
   imports =
     [
       ./hardware-configuration.nix
-      inputs.nixos-hardware.nixosModules.dell-precision-5530 # Moved from flake.nix
+      ../../modules/nixos/core.nix
+      ../../modules/nixos/workstation.nix
+      inputs.nixos-hardware.nixosModules.dell-precision-5530
     ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  hardware.graphics.enable = true;
-  hardware.nvidia.modesetting.enable = true;
 
-  networking.hostName = "charm"; # Define your hostname.
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  networking.hostName = "charm";
   networking.networkmanager.enable = true;
-
-  time.timeZone = "America/New_York";
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
 
   services.xserver.enable = true;
   services.displayManager.gdm.enable = true;
@@ -37,28 +22,19 @@
     layout = "us";
     variant = "";
   };
+  hardware.graphics.enable = true;
+  hardware.nvidia.modesetting.enable = true;
 
   services.printing.enable = true;
 
   virtualisation.docker.enable = true;
 
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  users.users.strange = {
+  users.users.${vars.username} = {
     isNormalUser = true;
-    description = "strange";
     extraGroups = [ "networkmanager" "wheel" "docker" ];
     packages = with pkgs; [ ];
   };
 
-  programs.nano.enable = false;
   programs.steam.enable = true;
   programs.hyprland = {
     enable = true;
@@ -68,24 +44,6 @@
   environment.sessionVariables = {
     WLR_NO_HARDWARE_CURSORS = "1";
     NIXOS_OZONE_WL = "1";
-  };
-
-  nixpkgs.config.allowUnfree = true;
-
-  environment.variables.EDITOR = "vim";
-  environment.systemPackages = with pkgs; [
-    vim
-    docker-compose
-  ];
-
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-  ];
-
-  services.pcscd.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
   };
 
   environment.gnome.excludePackages = with pkgs; [
