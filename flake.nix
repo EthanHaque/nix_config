b28131b8281cc2716bc3435d4dd157abf0b6d6c0
Ethan Haque <ethan.k.haque@gmail.com>
2026-02-04 20:51:46 -0500

    refactor: simplify flake glue code

--- flake.nix
+++ flake.nix
@@ -21,70 +21,46 @@
       # TODO: add flake to lazy config repo
       flake = false;
     };
-   };
+  };
 
-  outputs = inputs@{ nixpkgs, home-manager, nixos-hardware, nvim-lazy-config, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, nixos-hardware, ... }:
+    let
+# Helper function to create a host
+    mkHost = { hostname, system, username, extraModules ? [] }: nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = {
+        inherit inputs;
+        vars = { inherit username; };
+      };
+      modules = [
+        ./hosts/${hostname}
+      home-manager.nixosModules.home-manager
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users.${username} = import ./modules/home-manager/${username}.nix;
+        home-manager.extraSpecialArgs = { inherit inputs; vars = { inherit username; }; };
+        home-manager.backupFileExtension = "backup";
+      }
+      ] ++ extraModules;
+    };
+  in {
     nixosConfigurations = {
-      charm = nixpkgs.lib.nixosSystem rec {
-        specialArgs = {
-          inherit inputs;
-          vars = {
-            username = "strange";
-          };
-        };
-        system = "x86_64-linux";
-        modules = [
-          ./hosts/precision
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.strange = import ./modules/home-manager/strange.nix;
-            home-manager.extraSpecialArgs = specialArgs;
-            home-manager.backupFileExtension = "backup";
-          }
-          nixos-hardware.nixosModules.dell-precision-5530
-        ];
+      charm = mkHost {
+        hostname = "precision"; # Folder name in ./hosts/
+          system = "x86_64-linux";
+        username = "strange";
+        extraModules = [ nixos-hardware.nixosModules.dell-precision-5530 ]; # Remove this and put it in default.nix
       };
-      zone = nixpkgs.lib.nixosSystem rec {
-        specialArgs = {
-          inherit inputs;
-          vars = {
-            username = "tape";
-          };
-        };
+      zone = mkHost {
+        hostname = "adlink";
         system = "aarch64-linux";
-        modules = [
-          ./hosts/adlink
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.tape = import ./modules/home-manager/tape.nix;
-            home-manager.extraSpecialArgs = specialArgs;
-            home-manager.backupFileExtension = "backup";
-          }
-        ];
+        username = "tape";
       };
-      tau = nixpkgs.lib.nixosSystem rec {
-        specialArgs = {
-          inherit inputs;
-          vars = {
-            username = "neutrino";
-          };
-        };
+      tau = mkHost {
+        hostname = "powerspec";
         system = "x86_64-linux";
-        modules = [
-          ./hosts/powerspec
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.neutrino = import ./modules/home-manager/neutrino.nix;
-            home-manager.extraSpecialArgs = specialArgs;
-            home-manager.backupFileExtension = "backup";
-          }
-        ];
+        username = "neutrino";
       };
     };
   };
