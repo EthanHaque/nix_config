c21e94e70da8e76ae87885a195861cd723ad9407
Ethan Haque <ethan.k.haque@gmail.com>
2026-02-05 22:10:49 -0500

    feat: add new rofi config

--- modules/home-manager/modules/rofi/default.nix
+++ modules/home-manager/modules/rofi/default.nix
@@ -1,19 +1,19 @@
+{ pkgs, config, ... }:
 
-{
-pkgs,
-config,
-...
-}:
 {
   programs.rofi = {
     enable = true;
     theme = ./theme.rasi;
     extraConfig = {
+      modi = "drun,run";
       drun-display-format = "{name}";
-      drun-categories = "";
       drun-show-actions = true;
       drun-url-launcher = "xdg-open";
       drun-match-fields = "name,generic,exec,categories,keywords";
+
+      show-icons = true;
+      display-drun = "Apps";
+      display-run = "Run";
     };
   };
 }
