4354498b5e2e79ced2f044bb7f304977b2ccd7c9
Ethan Haque <ethan.k.haque@gmail.com>
2026-03-01 02:54:48 -0500

    fix: remove wildcard certificate

--- modules/nixos/caddy.nix
+++ modules/nixos/caddy.nix
@@ -3,39 +3,30 @@
 {
   services.caddy = {
     enable = true;
-
     virtualHosts = {
-      "*.home.arpa" = {
+      "omada.home.arpa" = {
         extraConfig = ''
           tls internal
-
-          @omada host omada.home.arpa
-          handle @omada {
-            reverse_proxy https://localhost:8043 {
-              transport http {
-                tls_insecure_skip_verify
-              }
-            }
+          reverse_proxy https://localhost:8043 {
+          transport http {
+            tls_insecure_skip_verify
           }
+      }
+      '';
+      };
 
+      "search.home.arpa" = {
+        extraConfig = ''
+          tls internal
+          reverse_proxy 10.50.20.10:8080
+          '';
+      };
 
-          @searxng host search.home.arpa
-          handle @searxng {
-            reverse_proxy 10.50.20.10:8080
-          }
-
-
-          @vaultwarden host vault.home.arpa
-          handle @vaultwarden {
-            reverse_proxy 10.50.25.10:8322 {
-              header_up X-Real-IP {remote_host}
-            }
-          }
-
-
-          # Useful for debugging resolution issues
-          handle {
-            respond "Service not found or Caddy is working!" 404
+      "vault.home.arpa" = {
+        extraConfig = ''
+          tls internal
+          reverse_proxy 10.50.25.10:8322 {
+            header_up X-Real-IP {remote_host}
           }
         '';
       };
