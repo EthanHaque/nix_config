ac374a58402a5ae83dfff1ff8bed180c7ec30cb6
Ethan Haque <ethan.k.haque@gmail.com>
2026-03-10 22:49:41 -0400

    fix: improve speed and remove mods

--- modules/nixos/minecraft.nix
+++ modules/nixos/minecraft.nix
@@ -1,22 +1,18 @@
-{ config, pkgs, lib, inputs, ... }:
+{ pkgs, inputs, ... }:
 
-let
-  inherit (inputs.nix-minecraft.lib) collectFilesAt;
-
-  homesteadModpack = pkgs.fetchPackwizModpack {
-    src = inputs.homestead-pack;
-    packHash = "sha256-DIc1/wRvHw0YJrmRUUtXIJSSUaXFeR/SpK49OUEk080=";
-  };
-in
 {
   imports = [ inputs.nix-minecraft.nixosModules.minecraft-servers ];
   nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
+
+  # Pin the Minecraft server to P-cores (threads 0-15) on the i7-14700.
+  # P-cores turbo to 5.3 GHz vs E-cores at 4.2 GHz
+  systemd.services.minecraft-server-jebbaserver.serviceConfig.AllowedCPUs = "0-15";
 
   services.minecraft-servers = {
     enable = true;
     eula = true;
 
-    servers.homestead = {
+    servers.jebbaserver = {
       enable = true;
       openFirewall = true;
       package = pkgs.fabricServers.fabric-1_20_1;
@@ -29,21 +25,20 @@ in
         "-XX:+AlwaysPreTouch"
         "-XX:+DisableExplicitGC"
         "-XX:+PerfDisableSharedMem"
-        "-XX:+UnlockExperimentalVMOptions"
-        "-XX:ConcGCThreads=4"
       ];
-
 
       serverProperties = {
         server-port = 25565;
         white-list = true;
         difficulty = "hard";
         gamemode = "survival";
-        motd = "Homestead on Nix";
+        motd = "jebbaserver on Nix";
         max-players = 10;
         view-distance = 10;
         simulation-distance = 8;
         level-seed = "jebba05";
+        sync-chunk-writes = false;
+        network-compression-threshold = 256;
       };
 
       whitelist = {
@@ -53,13 +48,6 @@ in
       operators = {
         "bbeeath" = "e82e11fa-74f8-4958-b1fa-e9f4d1357c95";
       };
-
-      symlinks = collectFilesAt homesteadModpack "mods"
-        // collectFilesAt homesteadModpack "patchouli_books";
-
-      files = collectFilesAt homesteadModpack "config"
-        // collectFilesAt homesteadModpack "kubejs"
-        // collectFilesAt homesteadModpack "scripts";
     };
   };
 }
